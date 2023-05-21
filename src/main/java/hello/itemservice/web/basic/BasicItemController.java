f6186808e166a5a00d4c3398abb0e3dd71e8f9df
dongHun <actgong@gmail.com>
2023-05-21 16:59:34 +0900

    refactor: RedirectAttributes를 이용한 redirection 개선

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -77,11 +78,20 @@ public class BasicItemController {
     }
 
     // PRG 개선
-    @PostMapping("/add")
+//    @PostMapping("/add")
     public String addItemV5(Item item){
-        //ModelAttribute 생략 가능하다 했으니 이것 또한 가능
         itemRepository.save(item);
         return "redirect:/basic/items/" + item.getId();
+    }
+
+    @PostMapping("/add")
+    public String addItemV6(Item item, RedirectAttributes redirectAttributes){
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        // 하위의 path variable redirectAttributes 이용해 치환 가능
+        // 나머지 attribute는 query parameter로 들어간다.
+        return "redirect:/basic/items/{itemId}";
     }
 
     @GetMapping("/{itemId}/edit")
